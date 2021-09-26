a051d53723344d136c1e67532ab190cd32155b5c
Krzysztof Gregorowicz <gregorowicz.k@gmail.com>
2021-09-26 17:24:29 +0200

    class method returning model object from mongo response

--- models.py
+++ models.py
@@ -5,7 +5,25 @@ class GameBase(BaseModel):
     producer: str = Field(..., title="Game producer", max_length=20)
 
 class GameIn(GameBase):
+    """
+    Game input model
+    """
     pass
 
 class GameOut(GameBase):
+    """
+    Game output model
+    """
     id: str = Field(None, title="Game ID")
+
+    @classmethod
+    def from_mongo_result(cls, data):
+        """
+        Tworzy obiekt tej klasy na podstawie odpowiedzi 
+        z mongodb
+        """
+        if not data:
+            return data
+        id = data.pop('_id', None)
+        return cls(**dict(data, id=str(id)))
+
